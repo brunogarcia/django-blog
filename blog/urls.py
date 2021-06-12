e1e9c3b0b5c704f4c233ba885b8b32b8663f1b37
Bruno Garcia <bruno@garciaechegaray.com>
2021-06-12 16:55:50 +0200

    feat: delete a post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import (
     BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
+    BlogDeleteView,
 )
 
 
@@ -12,4 +13,5 @@ urlpatterns = [
     path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogCreateView.as_view(), name='post_new'),
     path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
