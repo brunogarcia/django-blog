43729e0b8a9109f7b11678f73a2b47279c1322bb
Bruno Garcia <bruno@garciaechegaray.com>
2021-06-12 18:59:24 +0200

    chore: add Heroku config

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%o+ly0f6ypiai6(m+1h-$33g1vxi@icqgfd2q!1=o_*c=t1$do'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
